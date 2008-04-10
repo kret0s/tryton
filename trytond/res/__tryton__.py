f8cb447a10603e621fcb819e580b6da349209e43
Bertrand Chenal <bch@b2ck.com>
2008-04-10 17:01:17 +0200

    Guidelines. Again.

---
+++ trytond/res/__tryton__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Res',
+    'active': True,
+    'depends': ['ir'],
+    'xml': [
+        'res.xml',
+        'group.xml',
+        'user.xml',
+        'request.xml',
+        'ir.xml',
+        ]
+}
---
+++ trytond/version.py
@@ -0,0 +1,5 @@
+PACKAGE = "trytond"
+VERSION = "0.0.1"
+LICENSE = "GPL-2"
+WEBSITE = "http://www.tryton.org/"
+
