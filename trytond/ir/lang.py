cad7ee85aca837da090dd1bc6409e31bdee12037
C?dric Krier <ced@b2ck.com>
2008-07-03 00:26:23 +0200

    Re-apply missing changeset 1310cd55cd07

---
+++ trytond/ir/lang.py
@@ -0,0 +1,30 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+"Lang"
+from trytond.osv import fields, OSV
+
+
+class Lang(OSV):
+    "Language"
+    _name = "ir.lang"
+    _log_access = False
+    _description = __doc__
+    name = fields.Char('Name', required=True)
+    code = fields.Char('Code', required=True,
+            help="RFC 4646 tag: http://tools.ietf.org/html/rfc4646")
+    translatable = fields.Boolean('Translatable')
+    active = fields.Boolean('Active')
+    direction = fields.Selection([
+       ('ltr', 'Left-to-right'),
+       ('rtl', 'Right-to-left'),
+       ], 'Direction',required=True)
+
+    def default_active(self, cursor, user, context=None):
+        return 1
+
+    def default_translatable(self, cursor, user, context=None):
+        return 0
+
+    def default_direction(self, cursor, user, context=None):
+        return 'ltr'
+
+Lang()
