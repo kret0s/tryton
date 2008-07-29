9ec9740c04471cbd31a78f2172d77b71ecb67bc2
C?dric Krier <ced@b2ck.com>
2008-07-29 11:01:20 +0200

    Remove some size constraints Fix init.sql to match the objects

---
+++ trytond/webdav/__tryton__.py
@@ -0,0 +1,13 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+{
+    'name': 'WebDAV',
+    'category': 'Generic',
+    'active': True,
+    'depends': ['ir'],
+    'xml': [
+        'webdav.xml',
+    ],
+    'translation': [
+        'fr_FR.csv',
+    ],
+}
