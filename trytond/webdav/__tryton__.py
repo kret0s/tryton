eb22aee2e3a5b1bf5d4fe991aef5f6110e324ed5
Bertrand Chenal <bch@b2ck.com>
2008-10-23 17:16:10 +0200

    Use user language to translate request from cron failure

---
+++ trytond/webdav/__tryton__.py
@@ -0,0 +1,15 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+{
+    'name': 'WebDAV',
+    'category': 'Generic',
+    'active': True,
+    'depends': ['ir'],
+    'xml': [
+        'webdav.xml',
+    ],
+    'translation': [
+        'fr_FR.csv',
+        'de_DE.csv',
+        'es_ES.csv',
+    ],
+}
