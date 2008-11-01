8ee47301a04853281d417146fadca373b588c0fd
C?dric Krier <ced@b2ck.com>
2008-11-01 22:26:31 +0100

    Fix translation of views by modules for issue491

---
+++ trytond/workflow/__tryton__.py
@@ -0,0 +1,15 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of this repository contains the full copyright notices and license terms.
+{
+    'name': 'Workflow',
+    'category': 'Generic',
+    'active': True,
+    'depends': ['ir', 'res'],
+    'xml': [
+        'workflow.xml',
+        ],
+    'translation': [
+        'fr_FR.csv',
+        'de_DE.csv',
+        'es_ES.csv',
+    ],
+}
