f2f8885f960c7ebc62f00630a69ec191526ba674
C?dric Krier <ced@b2ck.com>
2009-02-26 00:31:00 +0100

    Fix Report _rpc must be added in __init__ and not in init for issue820

---
+++ trytond/model/fields/selection.py
@@ -0,0 +1,17 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+
+from trytond.model.fields.field import Field
+
+
+class Selection(Field):
+    _type = 'selection'
+
+    def __init__(self, selection, string='', sort=True, translate=True, **args):
+        super(Selection, self).__init__(string=string, **args)
+        if hasattr(selection, 'copy'):
+            self.selection = selection.copy()
+        else:
+            self.selection = selection
+        self.sort = sort
+        self.translate_selection = translate
